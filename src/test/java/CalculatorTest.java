f0fb7d8fda6790714da162b41c2dd25fe58a8434
Antonio Lorusso <mitictonylor@gmail.com>
2020-05-25 16:47:09 +0100

    add classes, add add() in calculator, test pass

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,22 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+
+    Calculator calculator;
+
+
+@Before
+public void before(){
+    calculator = new Calculator();
+}
+@Test
+    public void add(){
+    assertEquals(5, calculator.add(2,3));
+
+}
+
+
+}
